fc40197210adc80e386a696a949cf144e53e8dc1
Imran <imurokks@gmail.com>
2018-08-02 15:42:25 +0530

    Theming

--- js/arkantos.js
+++ js/arkantos.js
@@ -2,3 +2,18 @@
 var urlFetch = window.location.href;
 console.log("Current URL is :"+urlFetch);
 
+
+<script>
+window.onscroll = function() {
+  shrinklogo()
+};
+
+function shrinklogo() {
+  var logo = document.getElementById("logo")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    logo.style.width = '50%';
+  } else {
+    logo.style.width = '100%';
+  }
+}
+</script>
